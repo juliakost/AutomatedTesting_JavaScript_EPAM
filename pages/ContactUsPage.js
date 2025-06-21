93797b2210704594e444d32bfbb8cc271af7f769
Yuliia Kostitsyna <Yuliia_Kostitsyna@epam.com>
2025-06-22 01:46:36 +0200

    ContactPage  tests changed

--- pages/ContactUsPage.js
+++ pages/ContactUsPage.js
@@ -8,27 +8,35 @@ class ContactPage {
     // Locators
     this.contactHeader = page.locator(
       'span.museo-sans-light:has-text("Contact Us")'
-    );
+    ); 
+    this.inquiryReason = page.getByRole("combobox", {name: "Select the Reason for Your Inquiry*",});
+    this.reasonOptionsList = page.locator('.os-content').nth(1);
+    this.comboboxReasons = page.locator(`li.select2-results__option`);
     this.firstNameInput = page.getByLabel("First Name");
     this.lastNameInput = page.getByLabel("Last Name");
     this.emailInput = page.getByLabel("Email*");
     this.phoneInput = page.getByLabel("Phone*");
-    this.locationInput = page.getByLabel("Location");
+    this.locationInput = page.getByLabel("Location*");
     this.aboutEpam = page.getByRole("combobox", {
       name: "How did you hear about EPAM?",
     });
     this.comboboxOptions = page.locator(`li.select2-results__option`);
-    this.gdprCheckbox = page.locator('input[type="checkbox"][aria-required="true"]');
+    this.gdprCheckbox = page.locator(
+      'input[type="checkbox"][aria-required="true"]'
+    );
     this.submitButton = page.getByRole("button", { name: "Submit" });
-  } 
-  
+  }
+  async goto() {
+    await this.page.goto("https://www.epam.com/about/who-we-are/contact");
+  }
+
   // Check if the Contact Header is visible
   async verifyContactHeader() {
     await expect(this.contactHeader).toBeVisible();
     await expect(this.contactHeader).toHaveText(/Contact/);
   }
 
-  // Fill in "Ask Us Anything' form
+  // Fill in mandatory within "Ask Us Anything' form
   async personalInfoInput(firstName, lastName, email, phone, dropdownOption) {
     console.log(
       "Is firstNameInput visible?",
@@ -47,15 +55,22 @@ class ContactPage {
     await expect(this.locationInput).not.toBeEmpty;
     await this.aboutEpam.click();
     await this.comboboxOptions.filter({ hasText: dropdownOption }).click();
-    console.log("Is gdprCheckbox visible?",await this.gdprCheckbox.isVisible());  // Should be `true` if visible
-    console.log(await this.gdprCheckbox.isEnabled());  // Should be `true` if enabled
+    console.log(
+      "Is gdprCheckbox visible?",
+      await this.gdprCheckbox.isVisible()
+    );
+    console.log("Is gdprCheckbox is ON?", await this.gdprCheckbox.isEnabled());
     await this.gdprCheckbox.check({ force: true });
     await expect(this.gdprCheckbox).toBeChecked();
   }
 
-  // Click on a Sumbit of Ask Us Anything
   async clickSumbit() {
     await this.submitButton.click();
+  }
+
+  async clickSelectReason() {
+    await this.inquiryReason.click();
+    await this.reasonOptionsList.isVisible();
   }
 }
 
--- tests/contactUsFormPOM.test.js
+++ tests/contactUsFormPOM.test.js
@@ -1,28 +1,21 @@
-const { test, expect } = require("@playwright/test"); // Import Playwright test utilities
-const { ContactPage } = require("../pages/contactUsPage.js"); // Import the ContactPage POM class
+const { test, expect } = require("@playwright/test");
+const { ContactPage } = require("../pages/contactUsPage.js");
 
-// Wrap your test cases in a test.describe block
 test.describe('Contact Page "Ask Us Anything" form fill in Tests', () => {
   test.use({ storageState: "state.json" });
 
-  test.beforeEach(async ({ page }) => {
-    // Navigate to the "Contact Us" page
-    await page.goto("https://www.epam.com/about/who-we-are/contact");
-  });
-
-  test("should be possible to submit a Ask Us Anything form", async ({page,}) => {
-    // Step 1: Create an instance of the ContactPage class
+  test("should be possible to submit a Ask Us Anything form", async ({
+    page,
+  }) => {
     const contactPage = new ContactPage(page);
-
-    // Step 2: Verify that the "Contact Us" header is visible
+    await ContactPage.goto();
     await contactPage.verifyContactHeader();
 
-    // Step 3: Fill in the "Ask Us Anything" form
     const firstName = "John";
     const lastName = "Doe";
     const email = "john.doe@example.com";
     const phone = "+1234567890";
-    const whereDidYouHear = "Partner"; // Adjust depending on dropdown options
+    const whereDidYouHear = "Partner";
 
     await contactPage.personalInfoInput(
       firstName,
@@ -32,20 +25,41 @@ test.describe('Contact Page "Ask Us Anything" form fill in Tests', () => {
       whereDidYouHear
     );
 
-    // Step 4: Verify that filled info is preserved before  click on Submit
-
     await expect(firstName).not.toBeEmpty;
     await expect(lastName).not.toBeEmpty;
     await expect(email).not.toBeEmpty;
     await expect(phone).not.toBeEmpty;
     await expect(whereDidYouHear).not.toBeEmpty;
 
-    // Step 5: Submit the form
     await contactPage.clickSumbit();
 
-    //Step 6:for NON-Prod ent-t without CAPTCHA: should see "Thank you" text
+    //for NON-Prod ent-t without CAPTCHA: should see "Thank you" text
+    const thankYouMessage = page.locator("text=Thank you for contacting us.");
+    await expect(thankYouMessage).toBeVisible();
+  });
 
-    //const thankYouMessage = page.locator("text=Thank you for contacting us.");
-    //await expect(thankYouMessage).toBeVisible(); // Assert it's visible
+  test("possible to submit a Ask Us Anything form with non-default 'Reason for Your Inquiry' option", async ({
+    page,
+  }) => {
+    const contactPage = new ContactPage(page);
+    await contactPage.goto();
+    await contactPage.verifyContactHeader();
+    await contactPage.clickSelectReason();
+    await expect(contactPage.reasonOptionsList).toHaveText([
+    'General Information Request',
+    'Talk to Sales in North America',
+    'Talk to Sales in Continental Europe',
+    'Talk to Sales in the UK',
+    'Talk to Sales in Northern Europe',
+    'Talk to Sales in APAC',
+    'Talk to the Consulting Team',
+    'Press Inquiry',
+    'Careers',
+    'Employment Verification',
+    'Partner Relations',
+    'Investor Relations',
+    'Analyst Relations',
+    'Website Feedback'
+  ]);
   });
 });
