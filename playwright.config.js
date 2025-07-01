8801fe326e5f13f937389adfb9dffda33f4f3d51
Yuliia Kostitsyna <Yuliia_Kostitsyna@epam.com>
2025-07-01 13:50:18 +0200

    Increase globally  Test Timeout to 60s

--- playwright.config.js
+++ playwright.config.js
@@ -24,6 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
+   timeout: 60000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
@@ -41,11 +42,12 @@ export default defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
-/*
-      {
+
+    {
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] },
     },
+    /*
     {
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
